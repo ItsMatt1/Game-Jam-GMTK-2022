fa6f51be626edb8c5c1be65fed490a9e1be15b9f
Carlos Neves <carlos@neves.me>
2022-07-16 01:23:19 -0300

    feat: let player shoot small circle bullets

---
+++ GJ_Game/Assets/Shooting.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+  public Transform firePoint;
+  public GameObject bulletPrefab;
+
+  public float bulletForce = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+          Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+      GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+      Rigidbody2D rigidbody2D = bullet.GetComponent<Rigidbody2D>();
+      rigidbody2D.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
