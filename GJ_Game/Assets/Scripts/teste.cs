6a57649deab65581f795389c341a136bc365cead
Carlos Neves <carlos@neves.me>
2022-07-16 00:22:15 -0300

    feat: top-down 2D player with WASD movement and looking at mouse pointer

--- GJ_Game/Assets/Scripts/teste.cs
+++ GJ_Game/Assets/Scripts/teste.cs
@@ -6,6 +6,12 @@ public class teste : MonoBehaviour
 {
     private Rigidbody2D rigidbody2D;
 
+    public float movementSpeed = 5f;
+    public Camera camera;
+
+    Vector2 movement;
+    Vector2 mousePosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +21,19 @@ public class teste : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      movement.x = Input.GetAxisRaw("Horizontal"); // `A` or `D` key
+      movement.y = Input.GetAxisRaw("Vertical"); // `W` or `S` key
 
+      mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
-        rigidbody2D.AddForce(new Vector2(0f, 0.3f), ForceMode2D.Impulse);
+        // rigidbody2D.AddForce(new Vector2(0f, 0.3f), ForceMode2D.Impulse);
+        rigidbody2D.MovePosition(rigidbody2D.position + movementSpeed * movement * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = mousePosition - rigidbody2D.position;
+        float lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rigidbody2D.rotation = lookAngle;
     }
 }
