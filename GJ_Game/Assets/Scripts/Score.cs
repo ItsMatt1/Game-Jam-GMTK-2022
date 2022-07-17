300424de5b1aeed5cc819087c6f245a1b28a9d29
Carlos Neves <carlos@neves.me>
2022-07-16 23:50:28 -0300

    feat: add game score to HUD

---
+++ GJ_Game/Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public static Score instance;
+    public int bodyCount = 0;
+    public TextMeshProUGUI bodyCountText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      instance = this;
+      bodyCountText.text = $"BODY COUNT: {bodyCount}";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bodyCountText.text = $"BODY COUNT: {bodyCount}";
+    }
+}
