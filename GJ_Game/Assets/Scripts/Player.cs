451423b2cf1d70d02169ba8a1cd0db35322f3ba5
ItsMatt1 <matedits03216@gmail.com>
2022-07-16 21:21:28 -0300

    feat: added healthbar

--- GJ_Game/Assets/Scripts/Player.cs
+++ GJ_Game/Assets/Scripts/Player.cs
@@ -12,22 +12,38 @@ public class Player : MonoBehaviour
     Vector2 movement;
     Vector2 mousePosition;
 
+
+    public int maxHealth = 4;
+    public int currentHealth;
+
+    public HealthBar healthBar;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal"); // `A` or `D` key
-        movement.y = Input.GetAxisRaw("Vertical"); // `W` or `S` key
+        getPlayerInput();
+    }
 
-        mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
     }
 
     void FixedUpdate()
+    {
+        movePlayer();
+    }
+
+    void movePlayer()
     {
         // rigidbody2D.AddForce(new Vector2(0f, 0.3f), ForceMode2D.Impulse);
         rigidbody2D.MovePosition(rigidbody2D.position + movementSpeed * movement * Time.fixedDeltaTime);
@@ -35,5 +51,21 @@ public class Player : MonoBehaviour
         Vector2 lookDirection = mousePosition - rigidbody2D.position;
         float lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
         rigidbody2D.rotation = lookAngle;
+    }
+
+    void getPlayerInput()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal"); // `A` or `D` key
+        movement.y = Input.GetAxisRaw("Vertical"); // `W` or `S` key
+
+        mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            TakeDamage(1);
+        }
     }
 }
