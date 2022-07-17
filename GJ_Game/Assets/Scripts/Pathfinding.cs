27c3fd898097cbf0ec205761f9fb9df09d62345c
Cley Junior <cleyjunior96@gmail.com>
2022-07-17 20:28:34 -0300

    TodasAsMerdas

---
+++ GJ_Game/Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding
+{
+    private Grid<PathNode> grid;
+
+    public Pathfinding(int width, int height)
+    {
+        grid = new Grid<PathNode>(width, height, 10f, Vector3.zero, (Grid<PathNode> g, int x, int y) => new PathNode(g, x, y));
+
+    }
+}
---
+++ GJ_Game/Assets/Scripts/Testing.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Pathfinding pathfinding = new Pathfinding(10, 10);
+    }
+}
